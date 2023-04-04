79196437b9a43e5356ecfe8058e8e33326ffa2b2
charlie <crohitp9@gmail.com>
2023-04-04 21:37:31 +0530

    practice-april

--- src/main/kotlin/solved/leetcode/code_april/FindTheDifference.kt
+++ src/main/kotlin/solved/leetcode/code_april/FindTheDifference.kt
@@ -5,7 +5,10 @@ package solved.leetcode.code_april
  *
  *  *** Find The Difference
  *
- *  # Intuition
+ *  # Intuition :
+ *          create has set count of string in given two string then map second string count with first string count
+ *          if the count is 0 means string one didn't have that char in it return it
+ *          of if the count is greater than stirng one char count it the extra char are not present in string one so return it
  *
  *  # Approach :
  *
@@ -31,7 +34,6 @@ fun main() {
 }
 
 fun findTheDifference(s: String, k: String): Char {
-    var ans = ' '
 
     val sCount = mutableMapOf<Char, Int>()
     val kCount = mutableMapOf<Char, Int>()
@@ -48,10 +50,10 @@ fun findTheDifference(s: String, k: String): Char {
     kCount.forEach { (ch, count) ->
         val sCheck = sCount.getOrDefault(ch, 0)
         if (sCheck == 0 || count > sCheck) {
-            ans = ch
+            return ch
         }
     }
 
 
-    return ans
+    return ' '
 }
